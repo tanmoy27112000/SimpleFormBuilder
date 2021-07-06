e7a7bc648311ed4d6f6bb0608fc3c2fd3efdf78e
Tanmoy karmakar <tkarmakar27112000@gmail.com>
2021-07-06 13:21:21 +0530

    wip

--- lib/src/formWidget.dart
+++ lib/src/formWidget.dart
@@ -73,19 +73,24 @@ class _FormBuilderState extends State<FormBuilder> {
             Center(
               child: Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  height: 50,
-                  width: screenWidth(
-                    context: context,
-                    mulBy: widget.submitButtonWidth ?? 0.5,
-                  ),
-                  decoration: widget.submitButtonDecoration ??
-                      BoxDecoration(
-                        borderRadius: BorderRadius.circular(5),
-                        color: Colors.blue,
-                      ),
-                  child: Center(
-                    child: Text("Submit"),
+                child: InkWell(
+                  onTap: () {
+                    widget.onSubmit(getCompleteData());
+                  },
+                  child: Container(
+                    height: 50,
+                    width: screenWidth(
+                      context: context,
+                      mulBy: widget.submitButtonWidth ?? 0.5,
+                    ),
+                    decoration: widget.submitButtonDecoration ??
+                        BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          color: Colors.blue,
+                        ),
+                    child: Center(
+                      child: Text("Submit"),
+                    ),
                   ),
                 ),
               ),
