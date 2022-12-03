e616b6474d5bc69b2738dc346713e0415fb5d872
TheCre8tor <cre8tor.alexander@gmail.com>
2022-12-03 08:29:56 +0100

    Refactored form datetime component, #1

--- lib/src/screens/form_builder/page/form_builder.dart
+++ lib/src/screens/form_builder/page/form_builder.dart
@@ -5,6 +5,7 @@ import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:simple_form_builder/src/screens/form_builder/widgets/simple_checkbox.dart';
+import 'package:simple_form_builder/src/screens/form_builder/widgets/simple_datetime.dart';
 import 'package:simple_form_builder/src/screens/form_builder/widgets/simple_dropdown.dart';
 import 'package:simple_form_builder/src/screens/form_builder/widgets/simple_multiple.dart';
 
@@ -197,111 +198,15 @@ class _FormBuilderState extends State<FormBuilder> {
           descriptionTextDecoration: widget.descriptionTextDecoration,
         );
       case "datetime":
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(left: 16.0, top: 16),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  widget.showIcon
-                      ? iconContainer(widget.dateImage)
-                      : Container(),
-                  Container(
-                    width: MediaQuery.of(context).size.width * 0.9,
-                    child: Text(
-                        "${widget.showIndex ? "${checklistModel!.data![widget.index].questions!.indexOf(questions) + 1}. " : ""}${questions.title}"),
-                  ),
-                  DescriptionWidget(
-                    questions: questions,
-                    textStyle: widget.descriptionTextDecoration,
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 16.0),
-              child: Row(
-                children: <Widget>[
-                  Theme(
-                    data: ThemeData(),
-                    child: Builder(
-                      builder: (context) => CustomDropdown(
-                        onChanged: (val) {
-                          print(val);
-                        },
-                        onTap: () async {
-                          DateTime tempDate = await selectDate(context);
-
-                          if (questions.answer == null) {
-                            setState(() {
-                              questions.answer = tempDate;
-                            });
-                          } else {
-                            setState(() {
-                              questions.answer = DateTime(
-                                tempDate.year,
-                                tempDate.month,
-                                tempDate.day,
-                                questions.answer.hour,
-                                questions.answer.minute,
-                              );
-                            });
-                          }
-                        },
-                        title: questions.answer == null
-                            ? "DD-MM-YYYY"
-                            : dateFormater.format(questions.answer),
-                        // date != null ? dateFormater.format(date) : "DD-MM-YYYY",
-                        showImage: false,
-                        isRequired: false,
-                        width: screenWidth(context: context, mulBy: 0.4),
-                      ),
-                    ),
-                  ),
-                  CustomDropdown(
-                    onChanged: (val) {},
-                    onTap: () async {
-                      TimeOfDay tempTime = await selectTime(context);
-
-                      if (questions.answer == null) {
-                        setState(() {
-                          questions.answer = DateTime(
-                            DateTime.now().year,
-                            DateTime.now().month,
-                            DateTime.now().day,
-                            tempTime.hour,
-                            tempTime.minute,
-                          );
-                        });
-                      } else {
-                        setState(() {
-                          questions.answer = DateTime(
-                            questions.answer.year,
-                            questions.answer.month,
-                            questions.answer.day,
-                            tempTime.hour,
-                            tempTime.minute,
-                          );
-                        });
-                      }
-
-                      // reminderController.updateIschanged(true);
-                    },
-                    title: questions.answer != null
-                        ? formatTimeOfDay(
-                            TimeOfDay.fromDateTime(questions.answer))
-                        : "Hr:Mins",
-                    showImage: false,
-                    isRequired: false,
-                    width: screenWidth(context: context, mulBy: 0.3),
-                  ),
-                ],
-              ),
-            ),
-            remarkWidget(questions, remarks, widget.remarkImage),
-          ],
+        return SimpleDateTime(
+          questions: questions,
+          checklistModel: checklistModel,
+          showIndex: widget.showIndex,
+          dateImage: widget.dateImage,
+          remarkImage: widget.remarkImage,
+          index: widget.index,
+          showIcon: remarks,
+          descriptionTextDecoration: widget.descriptionTextDecoration,
         );
       case "time":
         return Column(
---
+++ lib/src/screens/form_builder/widgets/simple_datetime.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/material.dart';
+import 'package:simple_form_builder/src/shared/checklistModel.dart';
+import 'package:simple_form_builder/src/shared/constant.dart';
+
+import 'custom_dropdown.dart';
+import 'description_widget.dart';
+import 'simple_icon_container.dart';
+import 'simple_remark.dart';
+
+class SimpleDateTime extends StatefulWidget {
+  const SimpleDateTime({
+    Key? key,
+    required this.questions,
+    this.checklistModel,
+    required this.showIndex,
+    this.dateImage,
+    this.remarkImage,
+    required this.index,
+    required this.showIcon,
+    this.descriptionTextDecoration,
+  }) : super(key: key);
+
+  final Questions questions;
+  final ChecklistModel? checklistModel;
+  final bool showIndex;
+  final String? dateImage;
+  final String? remarkImage;
+  final int index;
+  final bool showIcon;
+  final TextStyle? descriptionTextDecoration;
+
+  @override
+  State<SimpleDateTime> createState() => _SimpleDateTimeState();
+}
+
+class _SimpleDateTimeState extends State<SimpleDateTime> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(left: 16.0, top: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              widget.showIcon
+                  ? IconContainer(icon: widget.dateImage)
+                  : Container(),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.9,
+                child: Text(
+                    "${widget.showIndex ? "${widget.checklistModel!.data![widget.index].questions!.indexOf(widget.questions) + 1}. " : ""}${widget.questions.title}"),
+              ),
+              DescriptionWidget(
+                questions: widget.questions,
+                textStyle: widget.descriptionTextDecoration,
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 16.0),
+          child: Row(
+            children: <Widget>[
+              Theme(
+                data: ThemeData(),
+                child: Builder(
+                  builder: (context) => CustomDropdown(
+                    onChanged: (val) {
+                      print(val);
+                    },
+                    onTap: () async {
+                      DateTime tempDate = await selectDate(context);
+
+                      if (widget.questions.answer == null) {
+                        setState(() {
+                          widget.questions.answer = tempDate;
+                        });
+                      } else {
+                        setState(() {
+                          widget.questions.answer = DateTime(
+                            tempDate.year,
+                            tempDate.month,
+                            tempDate.day,
+                            widget.questions.answer.hour,
+                            widget.questions.answer.minute,
+                          );
+                        });
+                      }
+                    },
+                    title: widget.questions.answer == null
+                        ? "DD-MM-YYYY"
+                        : dateFormater.format(widget.questions.answer),
+                    // date != null ? dateFormater.format(date) : "DD-MM-YYYY",
+                    showImage: false,
+                    isRequired: false,
+                    width: screenWidth(context: context, mulBy: 0.4),
+                  ),
+                ),
+              ),
+              CustomDropdown(
+                onChanged: (val) {},
+                onTap: () async {
+                  TimeOfDay tempTime = await selectTime(context);
+
+                  if (widget.questions.answer == null) {
+                    setState(() {
+                      widget.questions.answer = DateTime(
+                        DateTime.now().year,
+                        DateTime.now().month,
+                        DateTime.now().day,
+                        tempTime.hour,
+                        tempTime.minute,
+                      );
+                    });
+                  } else {
+                    setState(() {
+                      widget.questions.answer = DateTime(
+                        widget.questions.answer.year,
+                        widget.questions.answer.month,
+                        widget.questions.answer.day,
+                        tempTime.hour,
+                        tempTime.minute,
+                      );
+                    });
+                  }
+
+                  // reminderController.updateIschanged(true);
+                },
+                title: widget.questions.answer != null
+                    ? formatTimeOfDay(
+                        TimeOfDay.fromDateTime(widget.questions.answer))
+                    : "Hr:Mins",
+                showImage: false,
+                isRequired: false,
+                width: screenWidth(context: context, mulBy: 0.3),
+              ),
+            ],
+          ),
+        ),
+        RemarkWidget(
+          questions: widget.questions,
+          remark: widget.showIcon,
+          icon: widget.remarkImage,
+          onChanged: (value) {
+            widget.questions.remarkData = value;
+            setState(() {});
+          },
+        ),
+      ],
+    );
+  }
+}
